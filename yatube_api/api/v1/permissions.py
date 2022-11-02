e5eaf9a29efdc91c0efcf2f263bc935c39ec5f5c
Антон Сахаров <av.sakharov.888@gmail.com>
2022-11-02 19:07:04 +0300

    Second review

--- yatube_api/api/v1/permissions.py
+++ yatube_api/api/v1/permissions.py
@@ -5,6 +5,4 @@ class IsOwnerOrReadOnly(BasePermission):
     """Предоставляет права на осуществление опасных методов запроса
         только автору объекта."""
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.author_id == request.user.id
+        return request.method in SAFE_METHODS or obj.author_id == request.user.id
